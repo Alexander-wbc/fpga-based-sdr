982ef36b879211eed721a4f84d365e619c341d4d
wbc <2404553683@qq.com>
2026-03-11 17:57:34 +0800

    建立工程

---
+++ rtl/fpga_based_sdr/fpga_based_sdr.srcs/sources_1/new/usb3_test.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2026/03/11 17:57:05
+// Design Name: 
+// Module Name: usb3_test
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module usb3_test(
+
+    );
+endmodule
